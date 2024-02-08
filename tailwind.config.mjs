b7bef562afa01d2c435c5c87611d29babd6e534e
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-02-08 14:26:58 -0500

    feat(ui): Add mobile and desktop navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				serif: ['David Libre', 'serif'],
+			},
+			colors: {
+				'brand-green': {
+					DEFAULT: '#1e6b25',
+					soft: '#d4ded5'
+				}
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated')
+	],
 }
